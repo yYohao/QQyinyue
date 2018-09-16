689520415f20a8e626daa9541dee46f2bdc2ab78
yYoHao <429747705@qq.com>
2018-09-16 12:45:41 +0800

    fourth

--- js/index.js
+++ js/index.js
@@ -4,8 +4,36 @@ window.onload = function () {
     var pre = document.getElementsByClassName("pre")[0];
     var next = document.getElementsByClassName("next")[0];
     var rec_slider= document.getElementById("rec-slider-ul");
+    var currentIndex = 0;
+    var li_arr1 = [
+        {img: "source/1.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/2.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/3.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/4.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/5.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"}
+    ];
+    var li_arr2 = [
+        {img: "source/6.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/7.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/8.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/9.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
+        {img: "source/0.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"}
+    ];
 
     pre.onclick = function () {
+        var li;
+        console.log(111);
+        if (currentIndex === 0){
+            li = createRecommend(li_arr2);
+            console.log(li);
+            rec_slider.appendChild(li);
+            console.log(rec_slider);
+            currentIndex = 1;
+        } else {
+            li = createRecommend(li_arr1);
+            rec_slider.appendChild(li);
+            currentIndex = 0;
+        }
         slider(rec_slider,1200);
     };
 
@@ -56,8 +84,6 @@ window.onload = function () {
 function slider(obj,ftarget) {
     var begin = 0,end = ftarget,target = 20;
     var dir = begin > end ? -target : target;
-    console.log(end);
-    console.log(dir);
     var timer = setInterval(function () {
         obj.style.left = obj.offsetLeft + dir + 'px';
         begin = begin + dir;
@@ -66,4 +92,37 @@ function slider(obj,ftarget) {
         }
     },20);
 
+}
+
+function createRecommend(arr) {
+    var li = document.createElement("li");
+    for (var i=0; i<5; i++){
+        var slider_item = document.createElement("div");
+        slider_item.className = "rec-slider-item rec-slider-item-mg";
+        li.appendChild(slider_item);
+
+        var rec_image = document.createElement("div");
+        rec_image.className = "rec-image";
+
+        var image = document.createElement("img");
+        image.src = arr[i].img;
+        rec_image.appendChild(image);
+
+        var image_title = document.createElement("div");
+        image_title.className = "rec_image_title";
+
+        var p1 = document.createElement("p");
+        p1.innerText = arr[i].img_title;
+        image_title.appendChild(p1);
+        var p2 = document.createElement("p");
+        p2.className = "play_count";
+        p2.innerText = "播放量：" + arr[0].counts;
+
+        image_title.appendChild(p2);
+
+        slider_item.appendChild(image_title);
+        slider_item.appendChild(rec_image);
+    }
+
+    return li;
 }
