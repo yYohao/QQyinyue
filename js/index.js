b25f8b63f143a18dc60466b95861b7f8948d0b14
yYoHao <429747705@qq.com>
2018-09-16 21:50:50 +0800

    thirdDay

--- js/index.js
+++ js/index.js
@@ -4,44 +4,42 @@ window.onload = function () {
     var pre = document.getElementsByClassName("pre")[0];
     var next = document.getElementsByClassName("next")[0];
     var rec_slider= document.getElementById("rec-slider-ul");
-    var currentIndex = 0;
-    var li_arr1 = [
-        {img: "source/1.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/2.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/3.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/4.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/5.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"}
-    ];
-    var li_arr2 = [
-        {img: "source/6.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/7.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/8.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/9.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"},
-        {img: "source/0.jpg",img_title: "每日新歌：一起想象刘宇宁首支单曲",counts: "4.9万"}
-    ];
-
+    var current_mark = document.getElementById("current-mark");
+    var currentIndex = 1, nextIndex = 0;
+    //左箭头点击事件
     pre.onclick = function () {
-        var li;
-        console.log(111);
-        if (currentIndex === 0){
-            li = createRecommend(li_arr2);
-            console.log(li);
-            rec_slider.appendChild(li);
-            console.log(rec_slider);
-            currentIndex = 1;
-        } else {
-            li = createRecommend(li_arr1);
-            rec_slider.appendChild(li);
-            currentIndex = 0;
-        }
-        slider(rec_slider,1200);
+        pre_action(currentIndex,nextIndex,rec_slider,current_mark,
+            function (x, y) {
+                currentIndex = x;
+                nextIndex = y;
+            })
     };
-
+    //右箭头点击事件
     next.onclick = function () {
-        slider(rec_slider,-1200);
+        next_action(currentIndex,nextIndex,rec_slider,current_mark,
+            function (x, y) {
+                currentIndex = x;
+                nextIndex = y;
+            })
+    };
+    //标记点击事件
+    current_mark.children[0].onclick = function () {
+        if(currentIndex === 0){
+            pre_action(currentIndex,nextIndex,rec_slider,current_mark,
+                function (x, y) {
+                    currentIndex = x;
+                    nextIndex = y;
+                })
+        }
     };
 
-
+    current_mark.children[1].onclick = function() {
+        next_action(currentIndex,nextIndex,rec_slider,current_mark,
+            function (x, y) {
+                currentIndex = x;
+                nextIndex = y;
+            })
+    };
 
     recommend.mouseover(function () {
         $(".pre").animate({left:"0"},500);
@@ -81,48 +79,61 @@ window.onload = function () {
 
 };
 
-function slider(obj,ftarget) {
+
+/**
+ * 轮播动画
+ * @param obj1  {document}
+ * @param obj2  {document}
+ * @param ftarget   {number}
+ */
+function slider(obj1,obj2,ftarget) {
     var begin = 0,end = ftarget,target = 20;
     var dir = begin > end ? -target : target;
     var timer = setInterval(function () {
-        obj.style.left = obj.offsetLeft + dir + 'px';
+        obj1.style.left = obj1.offsetLeft + dir + 'px';
+        obj2.style.left = obj2.offsetLeft + dir + 'px';
         begin = begin + dir;
         if (Math.abs(begin) >= Math.abs(end)){
             clearInterval(timer);
         }
-    },20);
+    },10);
 
 }
 
-function createRecommend(arr) {
-    var li = document.createElement("li");
-    for (var i=0; i<5; i++){
-        var slider_item = document.createElement("div");
-        slider_item.className = "rec-slider-item rec-slider-item-mg";
-        li.appendChild(slider_item);
-
-        var rec_image = document.createElement("div");
-        rec_image.className = "rec-image";
-
-        var image = document.createElement("img");
-        image.src = arr[i].img;
-        rec_image.appendChild(image);
-
-        var image_title = document.createElement("div");
-        image_title.className = "rec_image_title";
-
-        var p1 = document.createElement("p");
-        p1.innerText = arr[i].img_title;
-        image_title.appendChild(p1);
-        var p2 = document.createElement("p");
-        p2.className = "play_count";
-        p2.innerText = "播放量：" + arr[0].counts;
-
-        image_title.appendChild(p2);
-
-        slider_item.appendChild(image_title);
-        slider_item.appendChild(rec_image);
+function pre_action(currentIndex,nextIndex , slider_dom, mark,callback) {
+    if (currentIndex === 1){
+        slider_dom.children[currentIndex].style.left = "0px";
+        console.log(slider_dom.children[currentIndex]);
+        slider(slider_dom.children[0], slider_dom.children[1],1200);
+        currentIndex = 0;
+        nextIndex = 1;
+    } else {
+        slider_dom.children[currentIndex].style.left = "0px";
+        console.log(slider_dom.children[currentIndex]);
+        slider(slider_dom.children[0], slider_dom.children[1],1200);
+        currentIndex = 1;
+        nextIndex = 0;
     }
+    callback(currentIndex, nextIndex);
+    mark.children[nextIndex].className = "current_mark mark_seleced";
+    mark.children[currentIndex].className = "current_mark";
+}
 
-    return li;
+function next_action(currentIndex,nextIndex , slider_dom, mark,callback) {
+    if (currentIndex === 1){
+        slider_dom.children[currentIndex].style.left = "2400px";
+        console.log(slider_dom.children[currentIndex]);
+        slider(slider_dom.children[0], slider_dom.children[1],-1200);
+        currentIndex = 0;
+        nextIndex = 1;
+    } else {
+        slider_dom.children[currentIndex].style.left = "2400px";
+        console.log(slider_dom.children[currentIndex]);
+        slider(slider_dom.children[0], slider_dom.children[1],-1200);
+        currentIndex = 1;
+        nextIndex = 0;
+    }
+    callback(currentIndex, nextIndex);
+    mark.children[nextIndex].className = "current_mark mark_seleced";
+    mark.children[currentIndex].className = "current_mark";
 }
