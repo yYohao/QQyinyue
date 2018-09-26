a6e1660f5193c14e333e48197ff53ef955b4ffb5
yYoHao <429747705@qq.com>
2018-09-26 21:06:47 +0800

    fifth day

--- js/index.js
+++ js/index.js
@@ -5,9 +5,13 @@ window.onload = function () {
     var new_songs_items = $(".new_songs_image");
     var pre = document.getElementsByClassName("pre")[0];
     var next = document.getElementsByClassName("next")[0];
+    var new_pre = document.getElementById("new-pre");
+    var new_next = document.getElementById("new-next");
     var rec_slider= document.getElementById("rec-slider-ul");
     var current_mark = document.getElementById("current-mark");
+    let all_new_songs = document.getElementById("all-new-songs");
     var currentIndex = 1, nextIndex = 0;
+    let new_currentIndex = 0, new_nextIndex = 0;
     //左箭头点击事件
     pre.onclick = function () {
         pre_action(currentIndex,nextIndex,rec_slider,current_mark,
@@ -43,6 +47,21 @@ window.onload = function () {
             })
     };
 
+    //新歌首发中箭头点击事件
+    new_pre.onclick = function(){
+        new_nextIndex = new_currentIndex + 1;
+        slider_move(new_currentIndex,new_nextIndex,all_new_songs,1200,(x)=>{
+            new_currentIndex = x;
+        })
+    };
+
+    new_next.onclick = function(){
+        new_nextIndex = new_currentIndex - 1;
+        slider_move(new_currentIndex,new_nextIndex,all_new_songs,1200,(x)=>{
+            new_currentIndex = x;
+        })
+    };
+
     //歌单推荐鼠标进入效果
     recommend.mouseover(function () {
         $("#song-recommend .pre").animate({left:"0"},500);
@@ -70,7 +89,6 @@ window.onload = function () {
     });
     //新歌首发鼠标进入效果
     new_songs.mouseover(function () {
-        console.log(111);
         $("#new-songs .pre").animate({left:"0"},500);
         $("#new-songs .next").animate({right:"0"},500);
         for (var j=0; j<new_songs_items.length; j++){
@@ -118,6 +136,14 @@ function slider(obj1,obj2,ftarget) {
 
 }
 
+/**
+ * 先前点击
+ * @param currentIndex
+ * @param nextIndex
+ * @param slider_dom
+ * @param mark
+ * @param callback
+ */
 function pre_action(currentIndex,nextIndex , slider_dom, mark,callback) {
     if (currentIndex === 1){
         slider_dom.children[currentIndex].style.left = "0px";
@@ -137,6 +163,14 @@ function pre_action(currentIndex,nextIndex , slider_dom, mark,callback) {
     mark.children[currentIndex].className = "current_mark";
 }
 
+/**
+ * 先后点击
+ * @param currentIndex
+ * @param nextIndex
+ * @param slider_dom
+ * @param mark
+ * @param callback
+ */
 function next_action(currentIndex,nextIndex , slider_dom, mark,callback) {
     if (currentIndex === 1){
         slider_dom.children[currentIndex].style.left = "2400px";
@@ -154,4 +188,55 @@ function next_action(currentIndex,nextIndex , slider_dom, mark,callback) {
     callback(currentIndex, nextIndex);
     mark.children[nextIndex].className = "current_mark mark_seleced";
     mark.children[currentIndex].className = "current_mark";
+}
+
+
+function slider_move(currentIndex, nextIndex,element,itemWidth,callback) {
+    let distanceIndex = currentIndex - nextIndex; //移动的单元数
+    let offsetLeft = element.offsetLeft;
+    let distance = distanceIndex * itemWidth + offsetLeft;
+    // console.log(offsetLeft,distance);
+    if (offsetLeft > distance) {
+        move(element, distance, 1);
+    }else {
+        move(element, distance, 0);
+    }
+
+    console.log(offsetLeft,distance);
+    currentIndex = nextIndex;
+    callback(currentIndex);
+}
+
+function move(element, target, flag) {
+    let begin = 0,speed = 20;
+    let timer;
+    begin = element.offsetLeft;
+    if (target < begin){ // -speed 向右走
+        speed = -speed;
+    }
+    timer = setInterval(()=>{
+        element.style.left = begin + "px";
+        begin += speed;
+
+        if (flag > 0){
+            if (Math.abs(begin) > Math.abs(target)){
+                clearInterval(timer);
+            }
+            if (element.offsetLeft < -3600){
+                element.style.left = "-3600px";
+                clearInterval(timer);
+            }
+        } else {
+            if (Math.abs(begin) < Math.abs(target)){
+                clearInterval(timer);
+            }
+            if (element.offsetLeft > 0){
+                element.style.left = "0px";
+                clearInterval(timer);
+            }
+        }
+
+    }, 10);
+
+
 }
